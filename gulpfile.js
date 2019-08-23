189fcd4ea6ea80c8bea2b356f526868c448ce9ec
Jonjo McKay <jonjo@jonjomckay.com>
2019-08-23 14:27:48 +0100

    AUTOMATION-76: Removed gzipping of resources

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,18 @@
 const gulp = require('gulp');
-const gzip = require('gulp-gzip');
 const clean = require('gulp-clean');
 
 gulp.task('clean', () => {
     return gulp.src('./dist/**/*.*', {read: false})
         .pipe(clean({force: true}));
 });
- 
+
 gulp.task('compress and copy vendor files', () => {
     return gulp
         .src('./vendor/**/*.*')
-        .pipe(gzip({ append: false }))
         .pipe(gulp.dest('./dist/js/vendor'));
 });
 
 gulp.task('build', gulp.series(
-    'clean', 
-    'compress and copy vendor files',
+    'clean',
+    'copy vendor files',
 ));
